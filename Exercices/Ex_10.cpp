416734425903af29477a3e099824d97c0911d0c8
Stefan Carpeliuc <stefan.carpeliuc@tible.com>
2025-12-21 21:17:25 +0100

    optimized ex_10.1

--- Exercices/Ex_10.cpp
+++ Exercices/Ex_10.cpp
@@ -3,6 +3,13 @@
 #include <sstream>
 #include <queue>
 
+struct Combination {
+  int start;
+  int depth;
+  uint32_t result;
+};
+
+
 void Ex_10::Toggle(uint32_t& bits, int i) {
   bits ^= (1u << i);
 }
@@ -25,70 +32,44 @@ void Ex_10::Run1(ifstream& input)
 	while(getline(input, line))
 	{
 		total += AnalyzeLine(line);
-		//cout << endl << "===================" << endl << endl;
   }
 	cout << "Ex_10: " << total << endl;
 }
-struct Combination {
-  vector<uint32_t> buttons;
-  int start;
-  uint32_t result;
-	//vector<int> buttonIndexes_debug;
-};
+
 int Ex_10::AnalyzeLine(const string& line)
 {
   uint32_t target = 0;
-  vector<uint32_t> round;
-  //cout << "Target: " << endl;
+  vector<uint32_t> schematics;
   ForEachContent(line, "[", "]", [&](string r) {
     for (size_t i = 0; i < r.size(); i++) {
       if (r[i] == '#') Toggle(target, i);
     }
-    //cout << ToBitString(target) << endl;
     });
-  //cout << "Buttons: " << endl;
 
   ForEachContent(line, "(", ")", [&](string r) {
-    round.push_back(0);
+    schematics.push_back(0);
     GetNumber(r, [&](int n) {
-      Toggle(round.back(), n);
+      Toggle(schematics.back(), n);
     });
-    //cout << ToBitString(round.back()) << " : " << round.back() << endl;
     });
-  // use bfs 
   queue<Combination> q;
+  q.push({ 0, 0, 0 });
 
-  q.push({ {}, 0, 0/*, {} */});
   while (!q.empty()) {
     Combination node = q.front();
     q.pop();
-    uint32_t guess = node.result ^ target;
 
-		string nodeStr = ToBitString(node.result);
-		string guessStr = ToBitString(guess);
+    for (int i = node.start; i < (int)schematics.size(); i++) {
+      uint32_t r = node.result ^ schematics[i];
 
-    for (int i = node.start; i < round.size(); i++) {
-      if ((node.result ^ round[i]) == target) {
-        /*cout << "FOUND with buttons: ";
-        for (auto& x : node.buttonIndexes_debug){
-					cout << x << " ";
-        }
-				cout << i << endl;
-				cout << "Result: " << endl << ToBitString(node.result ^ round[i]) << endl;*/
-        //cout  << node.buttons.size()
-				return node.buttons.size() + 1;
+      if (r == target) {
+        return node.depth + 1;
       }
-			vector<uint32_t> copy = node.buttons;
-			//vector<int> copy_indexes = node.buttonIndexes_debug;
-			copy.push_back(round[i]);
-      //copy_indexes.push_back(i);
-			uint32_t r = node.result ^ round[i];
 
-			//string rStr = ToBitString(r);
-      
-      q.push({ copy, i + 1, r/*, copy_indexes*/ });
+      q.push({ i + 1, node.depth + 1, r });
     }
   }
+
 	return 0;
 }
 
@@ -103,7 +84,6 @@ void Ex_10::ForEachContent(
   const function<void(const string&)>& callback
 )
 {
-  // Escape end delimiter for character class
   string escapedEnd = "\\" + e;
 
   string pattern =

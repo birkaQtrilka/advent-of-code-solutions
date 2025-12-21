4bd59313c0d3632f1f685c89ff68eb33c07d00da
Stefan Carpeliuc <stefan.carpeliuc@tible.com>
2025-12-21 11:15:23 +0100

    working on ex_9.2

--- Exercices/Ex_9.cpp
+++ Exercices/Ex_9.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <array>
 #include "../utils.h"
+#include <queue>
 
 struct Vec2 {
   int x;
@@ -38,56 +39,6 @@ void Ex_9::Run1(ifstream& input)
 
 }
 
-inline long long key(int x, int y) {
-  return ((long long)y << 32) | (unsigned long long)x;
-}
-
-inline bool get(const unordered_set<long long>& grid, int x, int y) {
-  return grid.count(key(x, y));
-}
-
-inline void setp(unordered_set<long long>& grid, int x, int y) {
-  grid.insert(key(x, y));
-}
-
-//--------------------------------------------
-// CONNECT EDGES (sparse version)
-//--------------------------------------------
-void Ex_9::Connect(unordered_set<long long>& grid, Vec2 a, Vec2 b)
-{
-  if (a.x == b.x) {
-    int dy = (b.y > a.y) ? 1 : -1;
-    for (int y = a.y; y != b.y; y += dy) {
-      setp(grid, a.x, y);
-    }
-    setp(grid, b.x, b.y);
-  }
-  else {
-    int dx = (b.x > a.x) ? 1 : -1;
-    for (int x = a.x; x != b.x; x += dx) {
-      setp(grid, x, a.y);
-    }
-    setp(grid, b.x, b.y);
-  }
-}
-
-bool Ex_9::IsInside(const vector<Vec2>& poly, Vec2 p)
-{
-  bool inside = false;
-  int n = poly.size();
-
-  for (int i = 0, j = n - 1; i < n; j = i++)
-  {
-    bool intersect =
-      ((poly[i].y > p.y) != (poly[j].y > p.y)) &&
-      (p.x < (poly[j].x - poly[i].x) * (p.y - poly[i].y)
-        / double(poly[j].y - poly[i].y) + poly[i].x);
-
-    if (intersect) inside = !inside;
-  }
-  return inside;
-}
-
 void Ex_9::AvailableBounds(int x, int y, int maxX, int maxY, vector<Vec2>& out)
 {
   out.clear();
@@ -97,50 +48,38 @@ void Ex_9::AvailableBounds(int x, int y, int maxX, int maxY, vector<Vec2>& out)
   if (y + 1 < maxY)  out.push_back({ x, y + 1 });
 }
 
-Vec2 Ex_9::GetFirstPointInsideLoop(vector<Vec2>& tmp,
-  const vector<Vec2>& poly,
-  Vec2 corner, int maxX, int maxY)
-{
-  tmp.clear();
+struct Rect {
+  Vec2 min;
+  Vec2 max;
+	long long area;
 
-  int x = corner.x;
-  int y = corner.y;
-
-  // check diagonals
-  if (x + 1 < maxX && y + 1 < maxY) tmp.push_back({ x + 1, y + 1 });
-  if (x > 0 && y > 0)               tmp.push_back({ x - 1, y - 1 });
-  if (x + 1 < maxX && y > 0)        tmp.push_back({ x + 1, y - 1 });
-  if (x > 0 && y + 1 < maxY)        tmp.push_back({ x - 1, y + 1 });
-
-  for (auto& p : tmp)
-    if (IsInside(poly, p)) return p;
-
-  return { -1, -1 };
-}
+  bool operator<(const Rect& other) const {
+    return area < other.area;
+  }
+};
 
 void Ex_9::Run2(ifstream& input)
 {
   vector<Vec2> positions;
   string line;
 
-  int maxX = 0, maxY = 0;
-
-  // read input
   while (getline(input, line)) {
     int x, y;
     sscanf_s(line.c_str(), "%d,%d", &x, &y);
 
     positions.push_back({ x, y });
   }
-  unordered_set<long long> grid;
-  for (size_t i = 1; i < positions.size(); i++) {
-    Connect(grid, positions[i - 1], positions[i]);
-  }
-  Connect(grid, positions[0], positions[positions.size() - 1]);
-  // bfs fill
+  // three for loops
+	// loop 1-2 - go through all pairs of points
+	// make rectangle
+  // 
+	// loop 3 - check if any other point is inside the rectangle formed by the pair
 
+  
 
-  long long best = -1;
+  std::priority_queue<Rect> pq;
+
+	long long smallestArea = INT_MAX;
 
   for (size_t i = 0; i < positions.size(); ++i)
   {
@@ -148,49 +87,48 @@ void Ex_9::Run2(ifstream& input)
     {
       Vec2 a = positions[i];
       Vec2 b = positions[j];
+      int w = abs(a.x - b.x) + 1;
+      int h = abs(a.y - b.y) + 1;
 
-      Vec2 c = { a.x, b.y };
-      Vec2 d = { b.x, a.y };
+			Vec2 min = { std::min(a.x, b.x), std::min(a.y, b.y) };
+			Vec2 max = { std::max(a.x, b.x), std::max(a.y, b.y) };
+      long long area = 1LL * w * h;
+
+      pq.push({min, max, area});
 
 
-      long long area =
-        1LL * (abs(a.x - b.x) + 1) *
-        (abs(a.y - b.y) + 1);
-
-      best = max(best, area);
     }
   }
+  long long best = -1;
 
-  cout << "Ex_9: " << best << endl;
-}
-void Ex_9::WriteFilledSVG(const unordered_set<long long>& filled,
-  int maxX, int maxY, int scale, string filename)
-{
-  ofstream out(filename);
-  out << "<svg xmlns='http://www.w3.org/2000/svg' fill='black' stroke='none'>";
-
-  for (auto& cell : filled)
-  {
-    int x = cell & 0xffffffff;
-    int y = cell >> 32;
-    out << "<rect x='" << x * scale
-      << "' y='" << y * scale
-      << "' width='" << scale
-      << "' height='" << scale << "' />\n";
+  /*while (!pq.empty()) {
+    Rect rect = pq.top();
+    pq.pop();
+		cout << "Rect: (" << rect.min.x << "," << rect.min.y << ") -> (" << rect.max.x << "," << rect.max.y << ") Area: " << rect.area << endl;
   }
+  return;*/
+  while (!pq.empty()) {
+    Rect rect = pq.top();
+    pq.pop();
+		bool hasInside = false;
+    for (size_t k = 0; k < positions.size(); ++k)
+    {
+      if(rect.area == 40) {
+        cout << "aaa";
+			}
+      Vec2 p = positions[k];
+      if(p.x > rect.min.x && p.x < rect.max.x &&
+          p.y >  rect.min.y && p.y < rect.max.y) {
+				hasInside = true;
+        break;
+			}
+    }
+    if (!hasInside) {
+			best = rect.area;
+      break;
+    }
+	}
 
-  out << "</svg>";
+  cout << "Ex_9 (b): " << best << endl;
 }
-void Ex_9::WriteSVG(const vector<Vec2>& positions, const string& filename) {
-  ofstream out(filename);
-  out << "<svg xmlns='http://www.w3.org/2000/svg' "
-    "stroke='black' fill='none'>\n";
 
-  out << "<polyline points='";
-  for (auto& p : positions)
-    out << p.x << "," << p.y << " ";
-  out << positions[0].x << "," << positions[0].y;  // close shape
-  out << "' stroke='black' stroke-width='1'/>\n";
-
-  out << "</svg>";
-}
--- Exercices/Ex_9.h
+++ Exercices/Ex_9.h
@@ -13,12 +13,7 @@ class Ex_9 : public Challenge
 public:
 
 	void Run1(ifstream& input) override;
-	void Connect(unordered_set<long long>& grid, Vec2 a, Vec2 b);
-	bool IsInside(const vector<Vec2>& poly, Vec2 p);
 	void AvailableBounds(int x, int y, int maxX, int maxY, vector<Vec2>& out);
-	Vec2 GetFirstPointInsideLoop(vector<Vec2>& tmp, const vector<Vec2>& poly, Vec2 corner, int maxX, int maxY);
 	void Run2(ifstream& input) override;
-	void WriteFilledSVG(const unordered_set<long long>& filled, int maxX, int maxY, int scale, string filename);
-	void WriteSVG(const vector<Vec2>& positions, const string& filename);
 };
 
